fd7525aa4d9142cf3cf76f52746dd55e10cc2e21
Evan You <yyx990803@gmail.com>
2015-11-04 17:15:36 -0500

    use cssnano instead of clean-css

--- test/expects/myth.js
+++ test/expects/myth.js
@@ -1,1 +1,1 @@
-var __vueify_style__ = require("vueify-insert-css").insert("a{color:#847AD1}pre{padding:10px;margin:20px}")
+var __vueify_style__ = require("vueify-insert-css").insert("a{color:#847ad1}pre{padding:10px;margin:20px}")
