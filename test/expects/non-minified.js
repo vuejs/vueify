79b1d957e2a8ca31459060df96982d5e4e5593eb
Evan You <yyx990803@gmail.com>
2016-01-05 15:52:05 -0500

    deindent code blocks

--- test/expects/non-minified.js
+++ test/expects/non-minified.js
@@ -1,2 +1,2 @@
-var __vueify_style__ = require("vueify-insert-css").insert("\n  html {\n    font-size: 20px;\n  }\n")
-;(typeof module.exports === "function"? module.exports.options: module.exports).template = "\n  <h1 :id=\"id\" @click=\"hi\">hello</h1>\n"
+var __vueify_style__ = require("vueify-insert-css").insert("\nhtml {\n  font-size: 20px;\n}\n")
+;(typeof module.exports === "function"? module.exports.options: module.exports).template = "\n<h1 :id=\"id\" @click=\"hi\">hello</h1>\n"
