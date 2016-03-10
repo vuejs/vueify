722f870332c9e5666bd1d896d49340bd859ce49b
Evan You <yyx990803@gmail.com>
2016-03-10 13:09:28 -0500

    fix scoped css test

--- test/expects/scoped.js
+++ test/expects/scoped.js
@@ -1,2 +1,2 @@
 var __vueify_style__ = require("vueify-insert-css").insert("div[{{id}}]{color:red}.test[{{id}}]{color:green}.test[{{id}}]:after{content:'bye!'}@media print{div[{{id}}]{color:green}}")
-;(typeof module.exports === "function"? module.exports.options: module.exports).template = "<div _v-31dda7ef=\"\">hi<p class=test _v-31dda7ef=\"\">bye</div><template v-if=ok><p class=test _v-31dda7ef=\"\">yo</template><svg _v-31dda7ef=\"\"><template><p _v-31dda7ef=\"\"></template></svg>"
+;(typeof module.exports === "function"? module.exports.options: module.exports).template = "<div {{id}}=\"\">hi<p class=test {{id}}=\"\">bye</div><template v-if=ok><p class=test {{id}}=\"\">yo</template><svg {{id}}=\"\"><template><p {{id}}=\"\"></template></svg>"
