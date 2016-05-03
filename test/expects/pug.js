20bd7f427eb0cdc721fd139daa6c18ad59de672f
Blade Barringer <blade@crookedneighbor.com>
2016-05-03 11:13:07 -0500

    Add pug support (#80)
    Closes #79

---
+++ test/expects/pug.js
@@ -0,0 +1,1 @@
+;(typeof module.exports === "function"? module.exports.options: module.exports).template = "<ul><li>foo<li>bar<li>baz</ul><ul><li>foo<li>bar<li>baz</ul>"
---
+++ test/fixtures/pug.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+//- Declaration
+mixin list
+  ul
+    li foo
+    li bar
+    li baz
+//- Use
++list
++list
+</template>
