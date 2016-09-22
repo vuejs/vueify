9f21360ff87e7a2548e3f6cbd111016c12ade282
Alex Bowers <bowersbros@gmail.com>
2016-09-22 01:15:25 +0100

    Start of Graphql work

---
+++ test/fixtures/graphql.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <span>Starwars Film: {{ film.title }}</span>
+    </div>
+</template>
+
+<graphql>
+{
+    film(filmID: 1) {
+        title
+        director
+    }
+}
+</graphql>
+
+<script>
+    export default {
+        props: ['film'],
+        data(graphql) {
+            return graphql
+                .success(function(data) {
+                    return {
+                        film: data.film
+                    }
+                })
+                .error(function(errors) {
+                    // Handle errors here
+                })
+        }
+    }
+</script>
