2117a82d6d1c053e88700abed1cccf95d43e1f7c
Mike <mike.grinspan@gmail.com>
2016-12-16 13:44:04 -0500

    Added callback support to plugins/extract-css

--- plugins/extract-css.js
+++ plugins/extract-css.js
@@ -7,18 +7,26 @@ module.exports = function (b, opts) {
   })
 
   var styles = Object.create(null)
-  var outPath = opts.out || opts.o || 'bundle.css'
+  var outCallback = opts.callback || opts.c || false
+  var outPath = opts.out || opts.o || (outCallback ? undefined : 'bundle.css')
 
   b.on('bundle', function (bs) {
     bs.on('end', function () {
       var css = Object.keys(styles)
         .map(function (file) { return styles[file] })
         .join('\n')
+      var callback = function () {
+        if(typeof outCallback === 'function') {
+          outCallback(css)
+        }
+      }
+
       if (typeof outPath === 'object' && outPath.write) {
-        outPath.write(css)
-        outPath.end()
+        outPath.end(css, callback)
       } else if (typeof outPath === 'string') {
-        fs.writeFile(outPath, css, function () {})
+        fs.writeFile(outPath, css, callback)
+      } else {
+        callback()
       }
     })
   })
